66dfd950a766c8f510093d716500f2474ef012bc
TimaFrolov <tima1911@yandex.ru>
2026-09-15 17:52:52 +0300

    chore(nix): switch to modern flake-compat approach

--- flake.nix
+++ flake.nix
@@ -1,23 +1,78 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
+    scr1 = {
+      url = "github:syntacore/scr1";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, systems, ... }:
-  let
-    eachSystem = fn:
-      nixpkgs.lib.genAttrs
-      (import systems)
-      (system: fn system nixpkgs.legacyPackages.${system});
-  in {
-    packages = eachSystem (system: pkgs: rec {
-      verihogg-format = pkgs.callPackage ./default.nix { inherit pkgs; };
-      default = verihogg-format;
-    });
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      scr1,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem =
+        { self', pkgs, ... }:
+        let
+          inherit (pkgs) lib;
+        in
+        {
+          packages.default = self'.packages.verihogg-format;
+          packages.verihogg-format = pkgs.stdenv.mkDerivation {
+            pname = "verihogg-format";
+            version = "0.1.0";
+            src = ./.;
 
-    devShells = eachSystem (system: pkgs: {
-      default = import ./shell.nix { inherit pkgs; };
-    });
-  };
+            meta = {
+              description = "SystemVerilog formatter powered by Slang";
+              license = lib.licenses.mit;
+              platforms = lib.platforms.linux;
+            };
+
+            nativeBuildInputs = with pkgs; [
+              gtest
+              cmake
+              ninja
+              pkg-config
+              microsoft-gsl
+            ];
+
+            buildInputs =
+              with pkgs;
+              [
+                sv-lang
+                microsoft-gsl
+              ]
+              ++ sv-lang.buildInputs;
+
+            preConfigure = ''
+              export SCR1_ROOT=${scr1}
+            '';
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs =
+              self'.packages.default.buildInputs
+              ++ self'.packages.default.nativeBuildInputs
+              ++ [ pkgs.llvmPackages_22.clang-tools ];
+
+            shellHook = ''
+              export SCR1_ROOT=${scr1}
+            '';
+          };
+        };
+    };
 }
--- shell.nix
+++ shell.nix
@@ -1,16 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  shared = import ./nix/shared.nix {
-    inherit pkgs;
-  };
-in
-pkgs.mkShell {
-  buildInputs =
-    shared.nativeBuildInputs
-    ++ shared.buildInputs
-    ++ shared.shellOnlyPackages;
-
-  shellHook = ''
-    export SCR1_ROOT=${shared.scr1}
-  '';
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      nodeName = lock.nodes.root.inputs.flake-compat;
+    in
+    fetchTarball {
+      url = lock.nodes.${nodeName}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+      sha256 = lock.nodes.${nodeName}.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
